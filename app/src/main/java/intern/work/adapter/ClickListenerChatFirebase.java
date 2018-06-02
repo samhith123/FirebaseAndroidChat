fd8f4d0f20b1d3ec97787bb203c86b5675b476d6
samhith.gardas123 <samhith.gardas123@gmail.com>
2018-06-03 00:03:36 +0530

    initial commit

---
+++ app/src/main/java/intern/work/adapter/ClickListenerChatFirebase.java
@@ -0,0 +1,13 @@
+package intern.work.adapter;
+
+import android.view.View;
+
+
+public interface ClickListenerChatFirebase {
+
+    void clickImageChat(View view, int position,String nameUser,String urlPhotoUser,String urlPhotoClick);
+
+
+    void clickImageMapChat(View view, int position,String latitude,String longitude);
+
+}
